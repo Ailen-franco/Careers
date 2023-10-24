79867657c798153f243051a0a0f51339633cca91
Ailen Franco <ailenyamilaf@hotmail.com>
2023-10-24 16:45:59 -0300

    add method POST from API

--- js/api.js
+++ js/api.js
@@ -11,5 +11,18 @@ getJobs = async() => {
     return await data
 };
 
+//Create Post
+const createPost = async(newJob) => {
+    showView("spinner");
+    console.log(newJob)
+    await fetch("https://6524bd60ea560a22a4ea0c28.mockapi.io/api/jobs/", {
+        method: "POST",
+        body: JSON.stringify(newJob),
+        headers:{ "content-type": "application/json; charset=UTF-8"},
+    });
+    getJobs();
+    clearForm();
+};
+
 
 
