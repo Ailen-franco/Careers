ee8e8de89b9f60788a496b89be25214843c8c8a0
Ailen Franco <ailenyamilaf@hotmail.com>
2023-10-24 16:22:47 -0300

    add fetch

--- js/api.js
+++ js/api.js
@@ -1,4 +1,15 @@
-
+getJobs = async() => {
+    showView("spinner");
+    let response = await fetch('https://6524bd60ea560a22a4ea0c28.mockapi.io/api/jobs');
+    let data = await response.json()
+    //Meter toda la parte html
+    //renderJobs(data)
+    
+    setTimeout(() => {
+        initialize(data);
+    }, 2000);
+    return await data
+};
 
 
 
